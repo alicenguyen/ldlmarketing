c6df2416c0e6a03410837e63af4be4f2989b491e
Alice <amnguyen30@gmail.com>
2012-08-20 21:13:46 -0700

    videos and photo buttons setup on the gallery tab

--- src/com/greendev/ldlmarketing/GalleryActivity.java
+++ src/com/greendev/ldlmarketing/GalleryActivity.java
@@ -1,6 +1,7 @@
 package com.greendev.ldlmarketing;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,11 +12,29 @@ public class GalleryActivity extends Activity implements OnClickListener{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gallery_layout);
+		
+		View photoButton = findViewById(R.id.button_photos);
+		photoButton.setOnClickListener(this);
+		
+		View videoButton = findViewById(R.id.button_videos);
+		videoButton.setOnClickListener(this);
+		
+		
 	}
 
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_photos:
+			Intent i = new Intent(this, PhotoActivity.class);
+			startActivity(i);
+			break;
+
+		case R.id.button_videos:
+			Intent c = new Intent(this, YoutubeActivity.class);
+			startActivity(c);
+			break;
+
+		}		
 	}
 	
 	
---
+++ src/com/greendev/ldlmarketing/PhotoActivity.java
@@ -0,0 +1,14 @@
+package com.greendev.ldlmarketing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhotoActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.photo_layout);
+	}
+
+}
