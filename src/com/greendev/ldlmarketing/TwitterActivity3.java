3b61dd91dd53bf6fb1c3ab6ddc394adb66512e72
Alice <amnguyen30@gmail.com>
2012-09-10 14:36:36 -0700

    Revert "twitter implementation still in progress"
    This reverts commit 82f310b9671d1ba38902b553c364c74cf5136a3a.

--- src/com/greendev/ldlmarketing/TwitterActivity3.java
+++ src/com/greendev/ldlmarketing/TwitterActivity3.java
@@ -1,18 +1,9 @@
 package com.greendev.ldlmarketing;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ListView;
 
 public class TwitterActivity3 extends Activity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.twitter3_layout);
-		
-		ListView listView = (ListView) findViewById(R.id.ListViewId);
-	
-	}
+
 
 }
