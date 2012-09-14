b3b2f36c57557214384fa15e166b108ca83f351f
Alice <amnguyen30@gmail.com>
2012-09-14 00:10:07 -0700

    portfolio buttons ui

--- src/com/greendev/ldlmarketing/PortfolioActivity.java
+++ src/com/greendev/ldlmarketing/PortfolioActivity.java
@@ -1,9 +1,11 @@
 package com.greendev.ldlmarketing;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class PortfolioActivity extends Activity implements OnClickListener {
 
@@ -11,6 +13,34 @@ public class PortfolioActivity extends Activity implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.portfolio_layout);
+
+		// Custom Font
+		Typeface font = Typeface.createFromAsset(getAssets(), "Eurosti.TTF");
+
+		// Buttons
+		View campaignsButton = findViewById(R.id.campaigns_button);
+		campaignsButton.setOnClickListener(this);
+		((TextView) campaignsButton).setTypeface(font);
+
+		View pressButton = findViewById(R.id.press_button);
+		pressButton.setOnClickListener(this);
+		((TextView) pressButton).setTypeface(font);
+
+		View gdButton = findViewById(R.id.graphic_design_button);
+		gdButton.setOnClickListener(this);
+		((TextView) gdButton).setTypeface(font);
+
+		View websitesButton = findViewById(R.id.websites_button);
+		websitesButton.setOnClickListener(this);
+		((TextView) websitesButton).setTypeface(font);
+
+		View videosButton = findViewById(R.id.videos_button);
+		videosButton.setOnClickListener(this);
+		((TextView) videosButton).setTypeface(font);
+
+		View dmButton = findViewById(R.id.digital_marketing_button);
+		dmButton.setOnClickListener(this);
+		((TextView) dmButton).setTypeface(font);
 	}
 
 	@Override
