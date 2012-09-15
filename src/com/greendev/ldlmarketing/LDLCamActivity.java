87fd44ebddda95a418e8613255471dd23c120be8
Alice <amnguyen30@gmail.com>
2012-09-15 01:44:23 -0700

    more UI

--- src/com/greendev/ldlmarketing/LDLCamActivity.java
+++ src/com/greendev/ldlmarketing/LDLCamActivity.java
@@ -1,7 +1,9 @@
 package com.greendev.ldlmarketing;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LDLCamActivity extends Activity {
 
@@ -9,6 +11,16 @@ public class LDLCamActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ldlcam_layout);
+
+		// Custom Font
+		Typeface font = Typeface.createFromAsset(getAssets(), "Eurosti.TTF");
+		
+		// Texts
+		TextView titleText1 = (TextView) findViewById(R.id.ldlcam_text1);
+		titleText1.setTypeface(font);
+
+		TextView titleText2 = (TextView) findViewById(R.id.ldlcam_text2);
+		titleText2.setTypeface(font);
 	}
 
 }
