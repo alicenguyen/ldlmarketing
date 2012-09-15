98b7491738d2439f757fae2a26b9b2155457ac97
Alice <amnguyen30@gmail.com>
2012-09-14 23:15:02 -0700

    figured how to customized title bar

--- src/com/greendev/ldlmarketing/AboutActivity.java
+++ src/com/greendev/ldlmarketing/AboutActivity.java
@@ -1,16 +1,29 @@
 package com.greendev.ldlmarketing;
    
+
+
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Custom Font
+		Typeface font = Typeface.createFromAsset(getAssets(), "Eurostib.TTF");
 		
-		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);	
 		setContentView(R.layout.about_layout);
-		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.about_title);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title);		
+		
+		TextView title = (TextView) findViewById(R.id.title);
+		title.setTypeface(font);
+		title.setText("About LDL");
+
+	
+		
 	}
 }
