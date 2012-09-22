2bb56fc66f5f091a664c92d7e27b574740ef2f43
Alice <amnguyen30@gmail.com>
2012-09-21 17:58:17 -0700

    "captions with ABBA baby"

--- src/com/greendev/image/Images.java
+++ src/com/greendev/image/Images.java
@@ -21,8 +21,25 @@ package com.greendev.image;
  */
 public class Images {
 	public final static String[] testImageDescriptions = new String[] {
-		"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-	};
+			"You can dance, you can jive, having the time of your life ",
+			"See that girl, watch that scene, diggin' the dancing queen ",
+			"Friday night and the lights are low ",
+			"Looking out for the place to go ",
+			"Where they play the right music, getting in the swing ",
+			"You come in to look for a king ", "Anybody could be that guy ",
+			"Night is young and the music's high ",
+			"With a bit of rock music, everything is fine ",
+			"You're in the mood for a dance ",
+			"And when you get the chance... ",
+			"You are the dancing queen, young and sweet, only seventeen ",
+			"Dancing queen, feel the beat from the tambourine ",
+			"You can dance, you can jive, having the time of your life ",
+			"See that girl, watch that scene, diggin' the dancing queen ",
+			"You're a teaser, you turn 'em on ",
+			"Leave them burning and then you're gone ",
+			"Looking out for another, anyone will do ",
+			"You're in the mood for a dance ",
+			"And when you get the chance... ", };
 
 	/**
 	 * Campaigns images
@@ -136,134 +153,125 @@ public class Images {
 			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Members-Only-Tweetie-De-Leon-Gonzales-Invite-150x150.jpg" };
 
 	public final static String[] graphicDesignsThumbUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Amber-Birthday-Poster-e1331471082612.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Amber-Happy-Hour-Poster-e1331471099249.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Amber-Lava-Fridays-Poster-e1331471120137.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Amber-Mixer-Poster-e1331471132717.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Lalaine-Art-Exhibit-Invite-e1331471141847.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Member-Only-Spotlight-Poster-e1329919129245.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Nisce-Skin-n-Face-Banner-e1331471155394.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Nisce-Skin-n-Face-Heart-to-Heart-Poster-e1331471166466.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Pasto-Friendly-Poster-e1331471175271.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Pasto-Friendly-Poster3-e1331471184167.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Trio-Gourmet-Treats-Invite-e1331471193385.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Trio-Fort-Business-Card-e1331471203449.png",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Triumph-Inspiration-Awards-Invite-Set.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Bean-Tantoco-Poster1-e1331471215407.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Members-Only-Tweetie-De-Leon-Gonzales-Invite.jpg"
-	};
-	
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Amber-Birthday-Poster-e1331471082612.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Amber-Happy-Hour-Poster-e1331471099249.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Amber-Lava-Fridays-Poster-e1331471120137.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Amber-Mixer-Poster-e1331471132717.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Lalaine-Art-Exhibit-Invite-e1331471141847.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Member-Only-Spotlight-Poster-e1329919129245.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Nisce-Skin-n-Face-Banner-e1331471155394.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Nisce-Skin-n-Face-Heart-to-Heart-Poster-e1331471166466.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Pasto-Friendly-Poster-e1331471175271.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Pasto-Friendly-Poster3-e1331471184167.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Trio-Gourmet-Treats-Invite-e1331471193385.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Trio-Fort-Business-Card-e1331471203449.png",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Triumph-Inspiration-Awards-Invite-Set.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Bean-Tantoco-Poster1-e1331471215407.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Members-Only-Tweetie-De-Leon-Gonzales-Invite.jpg" };
+
 	/**
 	 * Websites
 	 */
 	public final static String[] websitesImageUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Philfire-Website-150x150.png",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Bioessence-Website-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/contours-150x150.jpg",	
-	};
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Philfire-Website-150x150.png",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Bioessence-Website-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/contours-150x150.jpg", };
 	public final static String[] websitesThumbUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Philfire-Website-e1331465697715.png",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Bioessence-Website.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/contours.jpg",	
-	};
-	
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Philfire-Website-e1331465697715.png",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/Bioessence-Website.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2011/12/contours.jpg", };
+
 	/**
-	 *  Digital Marketing
+	 * Digital Marketing
 	 */
 	public final static String[] digitalMarketingImageUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Twitcon-Coco-Martin-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Twitcon-Piolo-Pascual-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Craft-Teaser-150x150.jpg",	
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Katsu-Teaser-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Scarlet-Teaser-150x150.jpghttp://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Scarlet-Teaser-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Sexy-Solutions-Poster-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Summer-2012-Exclusive-Coverage-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-1M41M-TwitterCampaign-150x150.jpg",
-	};
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Twitcon-Coco-Martin-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Twitcon-Piolo-Pascual-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Craft-Teaser-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Katsu-Teaser-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Scarlet-Teaser-150x150.jpghttp://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Scarlet-Teaser-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Sexy-Solutions-Poster-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Summer-2012-Exclusive-Coverage-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-1M41M-TwitterCampaign-150x150.jpg", };
 	public final static String[] digitalMarketingThumbUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Twitcon-Coco-Martin-e1332913374281.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Twitcon-Piolo-Pascual-e1332913386246.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Craft-Teaser-e1329919859563.jpg",	
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Katsu-Teaser-e1329919873575.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Scarlet-Teaser-e1329919901559.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Sexy-Solutions-Poster-e1329919915205.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Summer-2012-Exclusive-Coverage.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Be" +
-		"nch-1M41M-TwitterCampaign.jpg",
-	};
-	
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Twitcon-Coco-Martin-e1332913374281.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Twitcon-Piolo-Pascual-e1332913386246.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Craft-Teaser-e1329919859563.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Katsu-Teaser-e1329919873575.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Scarlet-Teaser-e1329919901559.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Sexy-Solutions-Poster-e1329919915205.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Bench-Summer-2012-Exclusive-Coverage.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Be"
+					+ "nch-1M41M-TwitterCampaign.jpg", };
+
 	/**
-	 *  Packaging
+	 * Packaging
 	 */
 	public final static String[] packagingImageUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Deodorant-Packaging-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Soap-Packaging-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Whitening-Soap-Packaging-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-N2-Derm+-Shopping-Bag-copy-150x150.jpg",	
-	};
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Deodorant-Packaging-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Soap-Packaging-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Whitening-Soap-Packaging-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-N2-Derm+-Shopping-Bag-copy-150x150.jpg", };
 	public final static String[] packagingThumbUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Deodorant-Packaging.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Soap-Packaging.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Whitening-Soap-Packaging.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-N2-Derm+-Shopping-Bag-copy.jpg",	
-	};
-	
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Deodorant-Packaging.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Soap-Packaging.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-Whitening-Soap-Packaging.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/01/Nisce-Skin-n-Face-N2-Derm+-Shopping-Bag-copy.jpg", };
+
 	/**
 	 * 
 	 */
 	public final static String[] boothDesignsImageUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-1-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-2-150x150.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-3-150x150.jpg",	
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-4-150x150.jpg",
-	};
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-1-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-2-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-3-150x150.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-4-150x150.jpg", };
 	public final static String[] boothDesignsThumbUrls = new String[] {
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-1.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-2.jpg",
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-3.jpg",	
-		"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-4.jpg",
-	};
-//	
-//	/**
-//	 * 
-//	 */
-//	public final static String[] ImageUrls = new String[] {
-//		"",
-//		"",
-//		"",	
-//	};
-//	public final static String[] ThumbUrls = new String[] {
-//		"",
-//		"",
-//		"",	
-//	};
-//	
-//	/**
-//	 * 
-//	 */
-//	public final static String[] ImageUrls = new String[] {
-//		"",
-//		"",
-//		"",	
-//	};
-//	public final static String[] ThumbUrls = new String[] {
-//		"",
-//		"",
-//		"",	
-//	};
-//	
-//	/**
-//	 * 
-//	 */
-//	public final static String[] ImageUrls = new String[] {
-//		"",
-//		"",
-//		"",	
-//	};
-//	public final static String[] ThumbUrls = new String[] {
-//		"",
-//		"",
-//		"",	
-//	};
-	
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-1.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-2.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-3.jpg",
+			"http://www.leungdeleonmarketing.com/wp-content/uploads/2012/03/N2-Derm-4.jpg", };
+	//
+	// /**
+	// *
+	// */
+	// public final static String[] ImageUrls = new String[] {
+	// "",
+	// "",
+	// "",
+	// };
+	// public final static String[] ThumbUrls = new String[] {
+	// "",
+	// "",
+	// "",
+	// };
+	//
+	// /**
+	// *
+	// */
+	// public final static String[] ImageUrls = new String[] {
+	// "",
+	// "",
+	// "",
+	// };
+	// public final static String[] ThumbUrls = new String[] {
+	// "",
+	// "",
+	// "",
+	// };
+	//
+	// /**
+	// *
+	// */
+	// public final static String[] ImageUrls = new String[] {
+	// "",
+	// "",
+	// "",
+	// };
+	// public final static String[] ThumbUrls = new String[] {
+	// "",
+	// "",
+	// "",
+	// };
+
 }
