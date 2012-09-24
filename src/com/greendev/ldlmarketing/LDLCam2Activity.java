1a0abd3b6648575c35a6edba2b021350a4ab14b2
Alice <amnguyen30@gmail.com>
2012-09-24 16:02:47 -0700

    working on ldlCam

---
+++ src/com/greendev/ldlmarketing/LDLCam2Activity.java
@@ -0,0 +1,25 @@
+package com.greendev.ldlmarketing;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class LDLCam2Activity extends Activity {
+	private String picPath;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ldlcam2_layout);
+		
+		// Get picture path from LDLCamActivity.class
+		Intent intent = getIntent();
+		picPath = intent.getStringExtra("PIC_PATH");
+		
+		// Load image view
+		ImageView imageView = (ImageView) findViewById(R.id.imgView);
+		imageView.setImageBitmap(BitmapFactory.decodeFile(picPath));
+	}
+}
--- src/com/greendev/ldlmarketing/LDLCamActivity.java
+++ src/com/greendev/ldlmarketing/LDLCamActivity.java
@@ -1,11 +1,22 @@
 package com.greendev.ldlmarketing;
 
+
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class LDLCamActivity extends Activity {
+public class LDLCamActivity extends Activity implements OnClickListener{
+	private static int RESULT_LOAD_IMAGE = 1;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -15,12 +26,56 @@ public class LDLCamActivity extends Activity {
 		// Custom Font
 		Typeface font = Typeface.createFromAsset(getAssets(), "Eurosti.TTF");
 		
-		// Texts
+		// Texts Views
 		TextView titleText1 = (TextView) findViewById(R.id.ldlcam_text1);
 		titleText1.setTypeface(font);
-
 		TextView titleText2 = (TextView) findViewById(R.id.ldlcam_text2);
 		titleText2.setTypeface(font);
+		
+		// Button View
+		Button pickButton = (Button) findViewById(R.id.pick_image_button);
+		pickButton.setOnClickListener(this);
 	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.pick_image_button:
+			Intent i = new Intent(
+					Intent.ACTION_PICK,
+					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			
+			startActivityForResult(i, RESULT_LOAD_IMAGE);
+			break;
+		
+	}
+	}
+	
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	super.onActivityResult(requestCode, resultCode, data);
+    	
+		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+			Cursor cursor = getContentResolver().query(selectedImage,
+					filePathColumn, null, null, null);
+			cursor.moveToFirst();
+
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+			
+			Intent i = new Intent(this, LDLCam2Activity.class);
+			i.putExtra("PIC_PATH", picturePath);
+			startActivity(i);
+			
+			
+		
+		}
+    
+    
+    }
 
 }
