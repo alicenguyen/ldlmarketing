0f81e81813694a61230fe63888e6695d3a5676f9
KiRiN <jasoncho0129@hotmail.com>
2012-11-24 18:33:54 -0800

    Update on photoFrame

--- src/com/greendev/ldlmarketing/LDLCamActivity.java
+++ src/com/greendev/ldlmarketing/LDLCamActivity.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import com.aviary.android.feather.FeatherActivity;
+import com.greendev.camera.FrameActivity;
 import com.greendev.ldlmarketing.R;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -20,7 +21,8 @@ import android.widget.TextView;
 
 public class LDLCamActivity extends Activity implements OnClickListener {
 	private static final int RESULT_LOAD_IMAGE = 1;
-	private static final int RESULT_CAMERA_IMAGE = 666;
+	private static final int RESULT_CAMERA_IMAGE = 2;
+	private static final int RESULT_FRAME_IMAGE = 3;
 	protected String _path;
 	protected String _output;
 
@@ -32,9 +34,7 @@ public class LDLCamActivity extends Activity implements OnClickListener {
 		_path = "temp.jpg";
 		_output = "output.jpg";
 		// Custom Font
-		Typeface font = Typeface.createFromAsset(getAssets(), 
-
-"Eurosti.TTF");
+		Typeface font = Typeface.createFromAsset(getAssets(), "Eurosti.TTF");
 
 		// Texts Views
 		TextView titleText1 = (TextView) findViewById(R.id.ldlcam_text1);
@@ -46,9 +46,7 @@ public class LDLCamActivity extends Activity implements OnClickListener {
 		Button pickButton = (Button) findViewById(R.id.pick_image_button);
 		pickButton.setOnClickListener(this);
 
-		Button cameraButton = (Button) findViewById
-
-(R.id.from_camera_button);
+		Button cameraButton = (Button) findViewById(R.id.from_camera_button);
 		cameraButton.setOnClickListener(this);
 	}
 
@@ -59,40 +57,25 @@ public class LDLCamActivity extends Activity implements OnClickListener {
 
 		// from gallery button
 		case R.id.pick_image_button:
-			Intent i = new Intent(
-					Intent.ACTION_PICK,
-					
+			Intent photoIntent = new Intent(Intent.ACTION_PICK, 
+					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-			startActivityForResult(i, RESULT_LOAD_IMAGE);
+			startActivityForResult(photoIntent, RESULT_LOAD_IMAGE);
 			break;
 
 		// from camera button
 		case R.id.from_camera_button:
-			Intent cameraIntent = new Intent(
-					
-
-android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+			Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 			try {
-
-				FileOutputStream fos = openFileOutput(_path,
-						
-
-Context.MODE_WORLD_WRITEABLE);
+				FileOutputStream fos = openFileOutput(_path, Context.MODE_WORLD_WRITEABLE);
 
 				fos.close();
 				File path = getFilesDir();
 				File file = new File(path, _path);
 				Uri uri = Uri.fromFile(file);
-				cameraIntent.putExtra
+				cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,	uri);
 
-(android.provider.MediaStore.EXTRA_OUTPUT,
-						uri);
-
-				startActivityForResult(cameraIntent, 
-
-RESULT_CAMERA_IMAGE);
+				startActivityForResult(cameraIntent, RESULT_CAMERA_IMAGE);
 			} catch (IOException ie) {
 
 			}
@@ -128,6 +111,21 @@ RESULT_CAMERA_IMAGE);
 
 				photoEditor(cameraImage);
 				break;
+				
+			case RESULT_FRAME_IMAGE:
+				Uri frameImage = null;
+				if (data != null) {
+					frameImage = data.getData();
+				} else {
+					File path = getFilesDir();
+
+					File file = new File(path, _output);
+					frameImage = Uri.fromFile(file);
+				}
+				
+				Intent j = new Intent(this, FrameActivity.class);
+				
+				break;
 
 			default:
 				break;
@@ -138,8 +136,8 @@ RESULT_CAMERA_IMAGE);
 	@SuppressLint("WorldWriteableFiles")
 	private void photoEditor(Uri photo) {
 
-		File path = getFilesDir();
-		File outFile = new File(path, _output);
+		//File path = getFilesDir();
+		//File outFile = new File(path, _output);
 
 		Intent i = new Intent(this, FeatherActivity.class);
 		// set the source image uri
@@ -149,7 +147,7 @@ RESULT_CAMERA_IMAGE);
 		// pass the uri of the destination image file (optional)
 		// This will be the same uri you will receive in the
 		// onActivityResult
-		i.putExtra("output", Uri.parse(outFile.getAbsolutePath()));
+		//i.putExtra("output", Uri.parse(outFile.getAbsolutePath()));
 		// format of the destination image (optional)
 		// newIntent.putExtra( "output-format",
 		// Bitmap.CompressFormat.JPEG.name() );
@@ -163,8 +161,8 @@ RESULT_CAMERA_IMAGE);
 		// enable fast rendering preview
 		i.putExtra("effect-enable-fast-preview", true);
 		// i.putExtra("hide-exit-unsave-confirmation", false);
-		startActivity(i);
-
+		startActivityForResult(i, RESULT_FRAME_IMAGE);
+		
 	}
 
 }
