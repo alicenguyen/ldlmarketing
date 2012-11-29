f58328a97c93f54bc413e35873b2d837af1d7a8e
KiRiN <jasoncho0129@hotmail.com>
2012-11-29 00:14:15 -0800

    update on frame layout

--- src/com/greendev/ldlmarketing/FrameActivity.java
+++ src/com/greendev/ldlmarketing/FrameActivity.java
@@ -19,8 +19,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.SyncStateContract.Constants;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
