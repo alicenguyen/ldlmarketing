d6c0a0144b6d23934edefa8f789200da35e42122
Alice <amnguyen30@gmail.com>
2012-11-29 15:05:21 -0800

    basic layout of 'ClientDirectActivity's is done.  Just need to include font and clickable url buttons

--- src/com/greendev/ldlmarketing/ClientDirectoryActivity.java
+++ src/com/greendev/ldlmarketing/ClientDirectoryActivity.java
@@ -2,33 +2,47 @@ package com.greendev.ldlmarketing;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ClientDirectoryActivity extends LDLActivity implements
 		OnClickListener {
-	String[] names;
-	GridView gridView;
+	//String[] NAMES;
+	
+	ListView listview;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.clientdir_layout);
-		// List View
-		names = new String[] { "Triumph", "LBC", "Bench", "Holcim", "brieo",
-				"Bioessence", "Pasto", "zao", "Bounty Fresh Chicken" };
 		
-		gridView = (GridView) findViewById(R.id.gridView1);
-		gridView.setAdapter(new ImageAdapter(this));
+		// Find the ListView resource.
+		listview = (ListView) findViewById(R.id.clientlistview);
+
+		Client client_data[] = new Client[] {
+				new Client(R.drawable.clientdir_triumph, NAMES[0], DESC[0]),
+				new Client(R.drawable.clientdir_lbc, NAMES[1], DESC[1]),
+				new Client(R.drawable.clientdir_bench, NAMES[2], DESC[2]),
+				new Client(R.drawable.clientdir_holcim, NAMES[3], DESC[3]),
+				new Client(R.drawable.clientdir_brieo, NAMES[4], DESC[4]),
+				new Client(R.drawable.clientdir_bioessence, NAMES[5], DESC[5]),
+				new Client(R.drawable.clientdir_pasto, NAMES[6], DESC[6]),
+				new Client(R.drawable.clientdir_zao, NAMES[7], DESC[7]),
+				new Client(R.drawable.clientdir_bountyfresh, NAMES[8], DESC[8]),
+
+		};
+
+		ClientAdapter adapter = new ClientAdapter(this,
+				R.layout.clientdir_item, client_data);
+
+		listview.setAdapter(adapter);
 	}
 
 	@Override
@@ -37,89 +51,84 @@ public class ClientDirectoryActivity extends LDLActivity implements
 
 	}
 
-	private class ImageAdapter extends BaseAdapter {
-		private Context context;
+	public class ClientAdapter extends ArrayAdapter<Client> {
+		Context context;
+		int layoutResourceId;
+		Client data[] = null;
 
-		public ImageAdapter(Context context) {
+		public ClientAdapter(Context context, int layoutResourceId,
+				Client[] data) {
+			super(context, layoutResourceId, data);
+			this.layoutResourceId = layoutResourceId;
 			this.context = context;
+			this.data = data;
 		}
 
+		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-	 
-			LayoutInflater inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	 
-			View gridView;
-	 
-				gridView = new View(context);
-	 
-				// get layout from mobile.xml
-				gridView = inflater.inflate(R.layout.clientdir_griditem, null);
-	 
-				// set value into textview
-				TextView textView = (TextView) gridView
-						.findViewById(R.id.clientdir_item_label);
-				textView.setText(" ");
-	 
-				// set image based on selected text
-				ImageView imageView = (ImageView) gridView
-						.findViewById(R.id.clientdir_item_image);
-	 
-				String title = names[position];
-	 
-				if(convertView == null){
-				switch(position){
-				case 0:
-					imageView.setImageResource(R.drawable.clientdir_triumph);
-					break;
-				case 1:
-					imageView.setImageResource(R.drawable.clientdir_lbc);
-					break;
-				case 2:
-					imageView.setImageResource(R.drawable.clientdir_bench);
-					break;
-				case 3:
-					imageView.setImageResource(R.drawable.clientdir_holcim);
-					break;
-				case 4:
-					imageView.setImageResource(R.drawable.clientdir_brieo);
-					break;
-				case 5:
-					imageView.setImageResource(R.drawable.clientdir_bioessence);
-					break;
-				case 6:
-					imageView.setImageResource(R.drawable.clientdir_pasto);
-					break;
-				case 7:
-					imageView.setImageResource(R.drawable.clientdir_zao);
-					break;
-				case 8:
-					imageView.setImageResource(R.drawable.clientdir_bountyfresh);
-					break;
-				}
-	 
+			View row = convertView;
+			ClientHolder holder = null;
+
+			if (row == null) {
+				LayoutInflater inflater = ((Activity) context)
+						.getLayoutInflater();
+				row = inflater.inflate(layoutResourceId, parent, false);
+
+				holder = new ClientHolder();
+				holder.imgIcon = (ImageView) row.findViewById(R.id.icon);
+				holder.txtName = (TextView) row.findViewById(R.id.name_view);
+				holder.txtDESC = (TextView) row.findViewById(R.id.desc_view);
+
+				row.setTag(holder);
 			} else {
-				gridView = (View) convertView;
+				holder = (ClientHolder) row.getTag();
 			}
-	 
-			return gridView;
-		}
 
-		@Override
-		public int getCount() {
-			return names.length;
-		}
+			Client client = data[position];
+			holder.txtName.setText(client.name);
+			holder.txtDESC.setText(client.DESC);
+			holder.imgIcon.setImageResource(client.icon);
 
-		@Override
-		public Object getItem(int position) {
-			return null;
-		}
-
-		@Override
-		public long getItemId(int position) {
-			return 0;
+			return row;
 		}
 
 	}
+
+	public class Client {
+		int icon;
+		String name;
+		String DESC;
+
+		public Client(int img, String name, String DESC) {
+			icon = img;
+			this.name = name;
+			this.DESC = DESC;
+		}
+	}
+
+	static class ClientHolder {
+		ImageView imgIcon;
+		TextView txtName;
+		TextView txtDESC;
+	}
+	
+	private static String[]  NAMES = new String[] { "Triumph", "LBC", "Bench", "Holcim", "brieo",
+		"Bioessence", "Pasto", "zao", "Bounty Fresh Chicken" };
+	
+	private static String[] DESC = new String[] {
+		"Triumph today enjoys a presence in over 120 countries all across the world. For its brands Triumph, sloggi, Valisere, the company develops, produces and sells underwear, lingerie, and swimwear, fusing excelling quality in workmanship, unrivaled fit and the latest in fashionable design.",
+		"One of the pioneers in the field, LBC is continuously innovating ways to send and receive remittances anytime, anywhere in the Philippines.  To date, LBC’s Remittance Service remains to be the safest and the fastest in the country.",
+		"A fashion brand that offers premium quality products at affordable prices. bench/ opened up as a small store selling men's t-shirts. The company has now grown to include a ladies' line, underwear, fragrances, housewares, snacks and other lifestyle products. The company's growth can be attributed to their use of celebrity endorsers, television commercials and giant billboards along the busy streets of the metro.",
+		"Holcim (US) Inc. is one of the largest manufacturers and suppliers of cement and mineral components in the United States.",
+		"Brieo, a product of Brieo Digital Media Group, Inc., is what we would like to call \"community-buyin\"",
+		"Bioessence Beauty Spa offers skin care and slimming services. Started in 1994 in Davao City with a three-bed clinic (Two for Facial and one for massage) . The founder aimed high and dreamed big-that BIOESSENCE become well known and be one of the leaders in the beauty, health and fitness industry.",
+		"This Italian restaurant is in the heart of busy, upscale Global City and perfect for sundowners (beer and a motley of cocktails rule) with friends after work. It has an appealing red and black colour scheme and the table layout/seating arrangement make it comfortable for family dinners. The staff are quick on the mark, gracious and deftly clear up questions on menu items.", 
+		"Zao is a bright, sleek and casually lively place where food is the main attraction. Vietnamese restaurants aren’t very common in the metro, and this level of authentic Vietnamese cooking is hard to come by. The food is good and easy on the wallet.",
+		"Bounty Fresh – Asia’s Best This is not just a slogan for a marketing gimmick. This is simply a statement of fact. Winner of numerous prestigious national awards in the poultry and livestock industry for the past several years, from product quality excellence all the way to consumer services, the Bounty Fresh Group upped the ante and bested the leading and world renowned poultry and livestock companies in Asia and won the 2009 Best Poultry and Livestock Company Award at the 2009 Asian Livestock Industry Awards held in Kuala Lumpur, Malaysia, in the categories of Technological Excellence, Food Safety, Marketing Excellence and Industry Leadership." };
+	
+	private static String[] URL = new String[] {
+		"http://www.triumph.com/ph/en/index.html", "http://www.lbcexpress.com/US/Home", "http://www.bench.com.ph/","http://www.holcim.us/","https://www.brieo.com/","http://www.bioessence.ph/","https://plus.google.com/104262187216332462787/about?gl=us&hl=en","http://www.asiatatlerdining.com/philippines/zao","http://www.bountyfreshchicken.com/"};
+
+
 
 }
