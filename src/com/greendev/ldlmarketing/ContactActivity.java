eeac853f57659b839480ce1ad98438b032155075
KiRiN <jasoncho0129@hotmail.com>
2012-12-08 02:58:41 -0800

    Calling function added

--- src/com/greendev/ldlmarketing/ContactActivity.java
+++ src/com/greendev/ldlmarketing/ContactActivity.java
@@ -1,27 +1,46 @@
 package com.greendev.ldlmarketing;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Typeface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class ContactActivity extends LDLActivity implements OnClickListener {
+	
+	private TextView phone;
+	private TextView emailT;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contactus_layout);
+		
+		phone = (TextView) findViewById(R.id.hotline);
+		phone.setOnClickListener(this);
+		
+		emailT = (TextView) findViewById(R.id.email);
+		emailT.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		
+		switch (v.getId()) {
+		case R.id.hotline:
+			
+	        String number = "tel:" + phone.getText().toString().trim();
+	        Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse(number)); 
+	        startActivity(callIntent);
+			
+		case R.id.email:
+		
+		}
 
 	}
 
 }
+
+
