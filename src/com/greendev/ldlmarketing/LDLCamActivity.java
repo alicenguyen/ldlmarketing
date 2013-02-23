d79e22c3a3154bac3a7fcd7b0b0300de53163edf
KiRiN <jasoncho0129@hotmail.com>
2013-02-23 11:06:18 -0800

    fixing S3 error in camera

--- src/com/greendev/ldlmarketing/LDLCamActivity.java
+++ src/com/greendev/ldlmarketing/LDLCamActivity.java
@@ -1,19 +1,25 @@
 package com.greendev.ldlmarketing;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.database.Cursor;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,7 +34,9 @@ public class LDLCamActivity extends Activity implements OnClickListener {
 	protected String _output;
 	protected File outFile;
 	protected File path;
-	private Uri imageFileUri;
+	//private Uri imageFileUri;
+	private Uri mImageCaptureUri;
+	private Uri mImageCaptureUri_samsung;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -74,19 +82,55 @@ public class LDLCamActivity extends Activity implements OnClickListener {
 
 		// from camera button
 		case R.id.from_camera_button:
-			Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-				imageFileUri = getOutputImageFileUri(this);
-				cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+			String storageState = Environment.getExternalStorageState();
 
-				startActivityForResult(cameraIntent, RESULT_CAMERA_IMAGE);
+			if (storageState.equals(Environment.MEDIA_MOUNTED)) {
+				Intent cameraIntent = new Intent(
+						MediaStore.ACTION_IMAGE_CAPTURE);
+
+				String filename = System.currentTimeMillis() + ".jpg";
+				ContentValues values = new ContentValues();
+				values.put(MediaStore.Images.Media.TITLE, filename);
+				mImageCaptureUri = getContentResolver().insert(
+						MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+				cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
+						mImageCaptureUri);
+
+				try {
+
+					startActivityForResult(cameraIntent, RESULT_CAMERA_IMAGE);
+				} catch (ActivityNotFoundException e) {
+					e.printStackTrace();
+				}
+			} else {
+				new AlertDialog.Builder(this)
+						.setMessage(
+								"External Storeage (SD Card) is required.\n\nCurrent state: "
+										+ storageState).setCancelable(true)
+						.create().show();
+				
+				/*
+				 * Intent cameraIntent = new
+				 * Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				 * 
+				 * try { imageFileUri = getOutputImageFileUri(this); } catch
+				 * (IOException e) { 
+				 * e.printStackTrace(); }
+				 * cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+				 * 
+				 * startActivityForResult(cameraIntent, RESULT_CAMERA_IMAGE);
+				 */
+			}
 
 			break;
 		}
 	}
 
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+	protected void onActivityResult(int requestCode, int resultCode,
+			final Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
 		if (resultCode == RESULT_OK) {
@@ -94,32 +138,204 @@ public class LDLCamActivity extends Activity implements OnClickListener {
 
 			case RESULT_LOAD_IMAGE:
 				if (data != null) {
-					/*Uri selectedImage = data.getData();
-					photoEditor(selectedImage);*/
-					
+
 					// performing task on a separate thread.
 					new Thread(new Runnable() {
-				        public void run() {
-				            Uri selectedImage = data.getData();
-				            photoEditor(selectedImage);
-				        }
-				    }).start();
+						public void run() {
+							Uri selectedImage = data.getData();
+							photoEditor(selectedImage);
+						}
+					}).start();
 				}
 				break;
 
 			case RESULT_CAMERA_IMAGE:
-				Uri cameraImage = null;
-				if (data != null) {
-					cameraImage = data.getData();
-				} else {
-					/*File path = getFilesDir();
+				Log.i("TAG", "Inside PICK_FROM_CAMERA");
 
-					File file = new File(path, _path);
-					cameraImage = Uri.fromFile(file);*/
-					cameraImage = imageFileUri;
+                // Final Code As Below
+                try {
+                    Log.i("TAG", "inside Samsung Phones");
+                    String[] projection = {
+                            MediaStore.Images.Thumbnails._ID, // The columns we want
+                            MediaStore.Images.Thumbnails.IMAGE_ID,
+                            MediaStore.Images.Thumbnails.KIND,
+                            MediaStore.Images.Thumbnails.DATA };
+                    String selection = MediaStore.Images.Thumbnails.KIND + "=" + // Select
+                                                                                    // only
+                                                                                    // mini's
+                            MediaStore.Images.Thumbnails.MINI_KIND;
+
+                    String sort = MediaStore.Images.Thumbnails._ID + " DESC";
+
+                    // At the moment, this is a bit of a hack, as I'm returning ALL
+                    // images, and just taking the latest one. There is a better way
+                    // to narrow this down I think with a WHERE clause which is
+                    // currently the selection variable
+                    Cursor myCursor = getContentResolver().query(
+                            MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+                            projection, selection, null, sort);
+
+                    long imageId = 0l;
+                    long thumbnailImageId = 0l;
+                    String thumbnailPath = "";
+
+                    try {
+                        myCursor.moveToFirst();
+                        imageId = myCursor
+                                .getLong(myCursor
+                                        .getColumnIndexOrThrow(MediaStore.Images.Thumbnails.IMAGE_ID));
+                        thumbnailImageId = myCursor
+                                .getLong(myCursor
+                                        .getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID));
+                        thumbnailPath = myCursor
+                                .getString(myCursor
+                                        .getColumnIndexOrThrow(MediaStore.Images.Thumbnails.DATA));
+                    } finally {
+                        // myCursor.close();
+                    }
+
+                    // Create new Cursor to obtain the file Path for the large image
+
+                    String[] largeFileProjection = {
+                            MediaStore.Images.ImageColumns._ID,
+                            MediaStore.Images.ImageColumns.DATA };
+
+                    String largeFileSort = MediaStore.Images.ImageColumns._ID
+                            + " DESC";
+                    myCursor = getContentResolver().query(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            largeFileProjection, null, null, largeFileSort);
+                    String largeImagePath = "";
+
+                    try {
+                        myCursor.moveToFirst();
+
+                        // This will actually give yo uthe file path location of the
+                        // image.
+                        largeImagePath = myCursor
+                                .getString(myCursor
+                                        .getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA));
+                        mImageCaptureUri_samsung = Uri.fromFile(new File(
+                                largeImagePath));
+                        mImageCaptureUri = null;
+                    } finally {
+                        // myCursor.close();
+                    }
+
+                    // These are the two URI's you'll be interested in. They give
+                    // you a
+                    // handle to the actual images
+                    Uri uriLargeImage = Uri.withAppendedPath(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            String.valueOf(imageId));
+                    Uri uriThumbnailImage = Uri.withAppendedPath(
+                            MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+                            String.valueOf(thumbnailImageId));
+
+                    // I've left out the remaining code, as all I do is assign the
+                    // URI's
+                    // to my own objects anyways...
+                } catch (Exception e) {
+                    mImageCaptureUri_samsung = null;
+                    Log.i("TAG",
+                            "inside catch Samsung Phones exception " + e.toString());
+
+                }
+
+                try {
+                    Log.i("TAG",
+                            "URI Samsung:" + mImageCaptureUri_samsung.getPath());
+
+                } catch (Exception e) {
+                    Log.i("TAG", "Excfeption inside Samsung URI :" + e.toString());
+                }
+
+                try {
+
+                    Log.i("TAG", "URI Normal:" + mImageCaptureUri.getPath());
+                } catch (Exception e) {
+                    Log.i("TAG", "Excfeption inside Normal URI :" + e.toString());
+                }
+
+				
+				/*String[] projection = {
+						MediaStore.Images.Thumbnails._ID, // The columns we want
+						MediaStore.Images.Thumbnails.IMAGE_ID,
+						MediaStore.Images.Thumbnails.KIND,
+						MediaStore.Images.Thumbnails.DATA };
+				String selection = MediaStore.Images.Thumbnails.KIND + "="
+						+ MediaStore.Images.Thumbnails.MINI_KIND;
+
+				String sort = MediaStore.Images.Thumbnails._ID + " DESC";
+
+				Cursor myCursor = getContentResolver().query(
+						MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+						projection, selection, null, sort);
+
+				long imageId = 0l;
+				long thumbnailImageId = 0l;
+				String thumbnailPath = "";
+
+				try {
+					myCursor.moveToFirst();
+					imageId = myCursor
+							.getLong(myCursor
+									.getColumnIndexOrThrow(MediaStore.Images.Thumbnails.IMAGE_ID));
+					thumbnailImageId = myCursor
+							.getLong(myCursor
+									.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID));
+					thumbnailPath = myCursor
+							.getString(myCursor
+									.getColumnIndexOrThrow(MediaStore.Images.Thumbnails.DATA));
+				} finally {
+					myCursor.close();
 				}
 
-				photoEditor(cameraImage);
+				// Create new Cursor to obtain the file Path for the large image
+
+				String[] largeFileProjection = {
+						MediaStore.Images.ImageColumns._ID,
+						MediaStore.Images.ImageColumns.DATA };
+
+				String largeFileSort = MediaStore.Images.ImageColumns._ID
+						+ " DESC";
+				myCursor = getContentResolver().query(
+						MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+						largeFileProjection, null, null, largeFileSort);
+				String largeImagePath = "";
+
+				try {
+					myCursor.moveToFirst();
+
+					// This will actually give you the file path location of the
+					// image.
+					largeImagePath = myCursor
+							.getString(myCursor
+									.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA));
+				} finally {
+					myCursor.close();
+				}
+				// These are the two URI's you'll be interested in. They give
+				// you a handle to the actual images
+				Uri uriLargeImage = Uri.withAppendedPath(
+						MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+						String.valueOf(imageId));
+				Uri uriThumbnailImage = Uri.withAppendedPath(
+						MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+						String.valueOf(thumbnailImageId));*/
+
+				/*
+				 * 
+				 * Uri cameraImage = null; if (data != null) { cameraImage =
+				 * data.getData(); } else { /* File path = getFilesDir();
+				 * 
+				 * File file = new File(path, _path); cameraImage =
+				 * Uri.fromFile(file);
+				 * 
+				 * cameraImage = imageFileUri; }
+				 */
+
+				photoEditor(mImageCaptureUri_samsung);
 				break;
 
 			case RESULT_FRAME_IMAGE:
@@ -141,6 +357,7 @@ public class LDLCamActivity extends Activity implements OnClickListener {
 
 	private void photoEditor(Uri photo) {
 
+		// System.out.println(photo);
 		File path = getFilesDir();
 		outFile = new File(path, _output);
 
@@ -200,13 +417,15 @@ public class LDLCamActivity extends Activity implements OnClickListener {
 		return cache.getAbsolutePath();
 	}
 
-	public static Uri getOutputImageFileUri(Context ctx) {
+	public static Uri getOutputImageFileUri(Context ctx) throws IOException {
 
 		String tstamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
 				.format(new Date());
 		File file = new File(getTempDirectoryPath(ctx), "IMG_" + tstamp
 				+ ".jpg");
-
+		if (!file.exists()) {
+			file.createNewFile();
+		}
 		return Uri.fromFile(file);
 
 	}
